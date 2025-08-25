93464f3ce8c760834b25ac259f23c9cc8845cfc6
root <root@ip-10-42-120-185.ec2.internal>
2025-08-25 14:20:33 +0000

    add

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,12 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+int _putchar( char c );
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
