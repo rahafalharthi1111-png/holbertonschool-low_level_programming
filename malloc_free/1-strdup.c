384e6b207fbcfb4238cb21fa6eb748d0e5beaf84
root <root@ip-10-42-67-243.ec2.internal>
2025-09-14 08:44:44 +0000

    add

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,28 +1,29 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- *
- * @str: a pointer to a char
- *
- * Return: returns a pointer
- */
-
+ * *_strdup - returns a pointer to a newly allocated space with copy of arg
+ * @str: string to copy
+ * Return: pointer to array or null
+ **/
 char *_strdup(char *str)
 {
-	char *t;
-	unsigned int i;
+	char *strDup;
+	int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	t = malloc(sizeof(char) * (strlen(str) + 1));
-	if (t == NULL)
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	strDup = malloc(sizeof(*str) * i);
+	if (strDup == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(str); i++)
-		t[i] = str[i];
-	t[i] = '\0';
-	return (t);
+	j = 0;
+	while (str[j] != '\0')
+	{
+		strDup[j] = str[j];
+		j++;
+	}
+	return (strDup);
 }
