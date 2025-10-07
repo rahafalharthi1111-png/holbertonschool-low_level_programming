dac86c95477a41b93437e88af37047e1ce2dbdb6
root <root@ip-10-42-201-92.ec2.internal>
2025-10-07 14:20:17 +0000

    add

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to duplicate and add to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
