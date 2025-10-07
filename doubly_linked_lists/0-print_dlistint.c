7ed0b64fdaad8d70e7c6bdaaf5bfcbfe7656515a
root <root@ip-10-42-201-92.ec2.internal>
2025-10-07 15:22:13 +0000

    add

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
+
 /**
- * print_dlistint - prints all elements of a dlistint_t list
+ * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the head of the list
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
     size_t count = 0;
+
     while (h != NULL)
     {
         printf("%d\n", h->n);
-        count++;
         h = h->next;
+        count++;
     }
 
     return (count);
