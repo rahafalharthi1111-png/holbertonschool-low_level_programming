c90c12681f04e32e58cfa060f0b0839b1d972335
root <root@ip-10-42-201-92.ec2.internal>
2025-10-07 15:35:18 +0000

    add

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return the number of elements in the list
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: pointer to the head of the list
  *
- * @h: pointer to the first node
- *
- * Return: returns the number of node
+ * Return: number of elements in the list
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
+		count++;
 		h = h->next;
-		n++;
 	}
-	return (n);
+
+	return (count);
 }
