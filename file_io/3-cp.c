5346a555c14fb6f20ef5af54f764eea4fb1b1ae5
root <root@ip-10-42-99-71.ec2.internal>
2025-10-08 10:14:44 +0000

    add

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,7 +5,7 @@
 #define BUFFER_SIZE 1024
 
 /**
- * error_exit - prints an error message to stderr and exits with a code
+ * error_exit - prints an error message and exits
  * @code: exit code
  * @msg: message format
  * @arg: argument for the message
@@ -17,7 +17,7 @@ void error_exit(int code, const char *msg, const char *arg)
 }
 
 /**
- * main - copies the content of one file to another
+ * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
  *
@@ -37,27 +37,33 @@ int main(int argc, char *argv[])
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+	{
+		close(fd_from);
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
 
 	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
-		if (w != r)
+		if (w == -1 || w != r)
+		{
+			close(fd_from);
+			close(fd_to);
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
 	}
+
 	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
 
 	return (0);
 }
