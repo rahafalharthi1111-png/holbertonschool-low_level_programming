0faa7d3b65355e434c7dbb1781792fdbc6cfc27a
root <root@ip-10-42-99-71.ec2.internal>
2025-10-08 10:18:48 +0000

    add

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,69 +1,113 @@
-#include "main.h"
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
- * error_exit - prints an error message and exits
- * @code: exit code
- * @msg: message format
- * @arg: argument for the message
+ * print_err_and_exit - print message to stderr and exit with code
  */
-void error_exit(int code, const char *msg, const char *arg)
+static void print_err_and_exit(int code, const char *fmt, ...)
 {
-	dprintf(STDERR_FILENO, msg, arg);
+	va_list ap;
+
+	va_start(ap, fmt);
+	vdprintf(STDERR_FILENO, fmt, ap);
+	va_end(ap);
 	exit(code);
 }
 
 /**
- * main - copies the content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success
+ * main - copy file_from to file_to
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
-	char buffer[BUFFER_SIZE];
+	int fd_from = -1, fd_to = -1;
+	ssize_t rlen, wlen;
+	char buf[BUF_SIZE];
+	ssize_t to_write, written;
+	char *buf_ptr;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
 	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
 	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(99);
+	}
+
+	while (1)
+	{
+		rlen = read(fd_from, buf, BUF_SIZE);
+		if (rlen == 0) /* EOF */
+			break;
+		if (rlen == -1)
 		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+			if (errno == EINTR)
+				continue; /* interrupted, retry read */
+			/* read error */
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			if (close(fd_from) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+			if (close(fd_to) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+			exit(98);
+		}
+
+		/* write loop to handle partial writes */
+		to_write = rlen;
+		buf_ptr = buf;
+		while (to_write > 0)
+		{
+			wlen = write(fd_to, buf_ptr, (size_t)to_write);
+			if (wlen == -1)
+			{
+				if (errno == EINTR)
+					continue; /* retry write on interrupt */
+				/* write error */
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				if (close(fd_from) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+				if (close(fd_to) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+				exit(99);
+			}
+			written = wlen;
+			to_write -= written;
+			buf_ptr += written;
 		}
 	}
 
-	if (r == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
+	/* close descriptors, checking errors and printing fd numbers */
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		if (close(fd_to) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 
 	return (0);
 }
